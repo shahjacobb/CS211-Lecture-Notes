0e3bb2ac9789efe10baf62ef63459cb396e51245
Shah Jacob <shahjacobb@gmail.com>
2022-04-11 01:54:25 -0400

    nty

---
+++ 8queens.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // chessboard
+
+    int chessboard[8][8] {0};
+    int r = 0, c = 0;
+
+    // row test
+    if (chessboard[r] == )
+    {
+
+    }  
+    // column test
+    if ()
+}
